21bac37e418c93894f7406697cd31512fc7052a4
Parv Jain <parvjain12@protonmail.com>
2025-10-04 23:35:49 +0530

    added rule for join ?

--- src/transpiler.py
+++ src/transpiler.py
@@ -22,14 +22,12 @@ class FlowTranspiler(Transformer):
     def BOOL_OPERATOR(self, op): return op.value
     def AGG_FUNC_NAME(self, n): return n.value
     def TYPE(self, t): return t.value
-
     @v_args(inline=True)
     def op(self, o): return o.value
     @v_args(inline=True)
     def pipe_step(self, item): return item
     @v_args(inline=True)
     def transformation(self, t): return t
-
     def field_decl(self, f): return (f[0], f[1])
     def env_var(self, e): return ('env', e[0][1:-1])
     def arguments(self, args): return {args[i]: args[i+1] for i in range(0, len(args), 2)}
@@ -40,114 +38,123 @@ class FlowTranspiler(Transformer):
         return None
 
     @v_args(inline=True)
-    def column_ref(self, table, column): return f"{{df}}['{column}']"
+    def column_ref(self, table, column): return (table, column) # Return a tuple of (table, col)
     def arith_expr(self, items): return f"({' '.join(str(i) for i in items)})"
     def term(self, items): return ' '.join(str(i) for i in items)
-    def factor(self, items): return str(items[0])
+    def factor(self, items):
+        # Handle column_ref tuples from the child
+        if isinstance(items[0], tuple):
+            table, column = items[0]
+            # Use placeholder for dataframe, to be replaced in pipeline/mutate
+            return f"{{df}}['{column}']"
+        return str(items[0])
     def bool_expression(self, items): return " & ".join(f"({item})" for item in items)
     
     def filter(self, f): return ('filter', f[0])
-    # UPDATED: No .value needed
     def select(self, s): return ('select', [name for name in s])
-    
-    # UPDATED: No .value needed
     def sort(self, s):
         columns, order = [], 'asc'
         for item in s:
             if item in ('asc', 'desc'): order = item
             else: columns.append(item)
         return ('sort', {'by': columns, 'ascending': (order == 'asc')})
-
     def mutate_expr(self, m): return {m[0]: m[1]}
     def mutate(self, m):
         all_mutations = {}
         for item in m: all_mutations.update(item)
         return ('mutate', all_mutations)
-        
     def group_by(self, g):
         return ('group_by', [name for name in g])
-
     def agg_function(self, a):
-        func_name = a[0]
-        col_name = a[1] if len(a) > 1 else None
+        func_name, *col_list = a
+        col_name = col_list[0] if col_list else None
         pandas_func_map = {'avg': 'mean'}
         pandas_func = pandas_func_map.get(func_name, func_name)
         return (col_name, pandas_func)
-
     def agg_expr(self, a):
         new_col_name, agg_func_tuple = a
         return (new_col_name, agg_func_tuple)
-        
     def aggregate(self, a):
         agg_dict = {new_col: (col, func) for new_col, (col, func) in a}
         return ('aggregate', agg_dict)
 
-    # UPDATED: No .value needed
-    def source_decl(self, s):
+    # --- FINAL: Real implementation for 'join' ---
+    def join_condition(self, j):
+        left_col_ref, right_col_ref = j
+        # left_col_ref is a tuple like ('users', 'city_id')
+        return {'left_on': left_col_ref[1], 'right_on': right_col_ref[1]}
+
+    def join_expr(self, j):
+        left_source, right_source, condition_result = j
+        
+        # Get the python variable names for the dataframes
+        left_df = self.variables.get(left_source)
+        right_df = self.variables.get(right_source)
+        
+        # Get the join keys from the condition
+        left_on = condition_result['left_on']
+        right_on = condition_result['right_on']
+
+        # Create a new variable for the result
+        new_py_var = self._new_temp_var()
+        
+        # Construct the pandas merge code
+        code_line = f"{new_py_var} = pd.merge({left_df}, {right_df}, left_on='{left_on}', right_on='{right_on}')"
+        
+        # Return the code and the name of the new variable
+        return ([code_line], new_py_var)
+
+    def source_decl(self, s): # ... (unchanged)
         flow_var, func_call, *schema_name_list = s
         python_var = f"{flow_var}_df"
         self.variables[flow_var] = python_var
         if schema_name_list:
-            schema_name = schema_name_list[0] # <-- FIX was here
+            schema_name = schema_name_list[0]
             if schema_name in self.schemas: self.variable_schemas[flow_var] = schema_name
             else: raise Exception(f"Error: Schema '{schema_name}' not defined.")
-
         if func_call['name'] == 'File':
             path = func_call['args']['path'][1:-1]
             self.code_blocks.append(f"{python_var} = pd.read_csv('{path}')")
         elif func_call['name'] == 'Postgres':
-            # ... (Postgres logic is unchanged)
             self.imports.add("create_engine from sqlalchemy")
             args = func_call['args']
             password = args['password']
             if isinstance(password, tuple) and password[0] == 'env': password_py_expr = f"os.getenv('{password[1]}')"
             else: password_py_expr = f"{password}"
-            user = args['user'][1:-1]
-            host = args['host'][1:-1]
-            database = args['database'][1:-1]
-            table = args['table'][1:-1]
+            user, host = args['user'][1:-1], args['host'][1:-1]
+            database, table = args['database'][1:-1], args['table'][1:-1]
             conn_str = f"'postgresql+psycopg2://{user}:' + {password_py_expr} + '@{host}/{database}'"
             code = f"""engine = create_engine({conn_str})\n{python_var} = pd.read_sql_table('{table}', engine)"""
             self.code_blocks.append(code)
 
-    def sink_decl(self, s):
+    def sink_decl(self, s): # ... (unchanged)
         name, func_call = s[0], s[1]
         self.sinks[name] = func_call
 
-    # UPDATED: No .value needed
-    def pipeline(self, p):
+    def pipeline(self, p): # ... (unchanged)
         start_flow_var, *steps = p
-        start_py_var = self.variables.get(start_flow_var) # <-- FIX was here
+        start_py_var = self.variables.get(start_flow_var)
         if not start_py_var: raise Exception(f"Error: Variable '{start_flow_var}' not defined.")
-        
         code, current_py_var, is_grouped = [], start_py_var, False
-        
-        # This part of the logic now depends on having self.group_by_cols available from the validator.
-        # This is a bit of a design flaw, but we'll proceed for now. A better design would pass state explicitly.
         self.group_by_cols = [] 
-        
         for item in steps:
             next_py_var = self._new_temp_var()
             if isinstance(item, tuple):
                 op_type, op_args = item
-                if op_type == 'filter':
-                    code.append(f"{next_py_var} = {current_py_var}[{op_args.replace('{df}', current_py_var)}]")
-                elif op_type == 'select':
-                    code.append(f"{next_py_var} = {current_py_var}[{op_args}]")
-                elif op_type == 'sort':
-                    code.append(f"{next_py_var} = {current_py_var}.sort_values(by={op_args['by']}, ascending={op_args['ascending']})")
+                if op_type == 'filter': code.append(f"{next_py_var} = {current_py_var}[{op_args.replace('{df}', current_py_var)}]")
+                elif op_type == 'select': code.append(f"{next_py_var} = {current_py_var}[{op_args}]")
+                elif op_type == 'sort': code.append(f"{next_py_var} = {current_py_var}.sort_values(by={op_args['by']}, ascending={op_args['ascending']})")
                 elif op_type == 'mutate':
                     assign_args = ", ".join([f"{k}={v.replace('{df}', current_py_var)}" for k,v in op_args.items()])
                     code.append(f"{next_py_var} = {current_py_var}.assign({assign_args})")
                 elif op_type == 'group_by':
-                    self.group_by_cols = op_args # Store for aggregate step
+                    self.group_by_cols = op_args
                     code.append(f"{next_py_var} = {current_py_var}.groupby({op_args})")
                     is_grouped = True
                 elif op_type == 'aggregate':
                     if not is_grouped: raise Exception("Error: 'aggregate' must be preceded by a 'group_by'.")
                     agg_args_list = []
                     for new_col, (src_col, func) in op_args.items():
-                        # Use the first group_by column for a column-less count()
                         col_for_count = self.group_by_cols[0] if self.group_by_cols else 'UNKNOWN_COLUMN'
                         agg_col = src_col if src_col is not None else col_for_count
                         agg_args_list.append(f"{new_col}=pd.NamedAgg(column='{agg_col}', aggfunc='{func}')")
@@ -163,23 +170,23 @@ class FlowTranspiler(Transformer):
                     code.append(f"{current_py_var}.to_csv('{path}', index=False)")
         return (code, current_py_var)
 
-    # UPDATED: No .value needed
-    def assignment(self, a):
-        flow_var, pipeline_result = a
-        pipeline_code, last_py_var = pipeline_result
+    def assignment(self, a): # ... (unchanged)
+        flow_var, expression_result = a
+        pipeline_code, last_py_var = expression_result
         new_py_var = f"{flow_var}_df"
         self.variables[flow_var] = new_py_var
-        pipeline_code.append(f"{new_py_var} = {last_py_var}")
+        if pipeline_code:
+            pipeline_code.append(f"{new_py_var} = {last_py_var}")
         comment = f"# Pipeline for '{flow_var}'"
         self.code_blocks.append(f"\n{comment}\n" + "\n".join(pipeline_code))
         
-    def execution(self, e):
+    def execution(self, e): # ... (unchanged)
         pipeline_result, = e
         pipeline_code, _ = pipeline_result
         comment = f"# Standalone pipeline execution"
         self.code_blocks.append(f"\n{comment}\n" + "\n".join(pipeline_code))
 
-    def start(self, s):
+    def start(self, s): # ... (unchanged)
         import_statements = sorted(list(self.imports), key=lambda x: " from " in x)
         header = "\n".join(f"import {imp}" if " from " not in imp else f"from {imp.split(' from ')[1]} import {imp.split(' from ')[0]}" for imp in import_statements)
         final_blocks = [b.strip() for b in self.code_blocks if b]
--- src/validator.py
+++ src/validator.py
@@ -10,42 +10,91 @@ class Validator(Visitor):
         self.current_schema_name = "unknown"
         self.group_by_cols = []
 
+    def assignment(self, tree):
+        new_var_name = tree.children[0].value
+        rhs_node = tree.children[1]
+        self.visit(rhs_node)
+        if rhs_node.data == 'join_expr':
+            left_source_name = rhs_node.children[0].value
+            right_source_name = rhs_node.children[1].value
+            left_schema_name = self.variable_schemas[left_source_name]
+            right_schema_name = self.variable_schemas[right_source_name]
+            merged_schema = self.schemas[left_schema_name].copy()
+            merged_schema.update(self.schemas[right_schema_name])
+            new_schema_name = f"join_{left_schema_name}_{right_schema_name}"
+            self.schemas[new_schema_name] = merged_schema
+            self.variable_schemas[new_var_name] = new_schema_name
+        elif rhs_node.data == 'pipeline':
+            start_var_name = rhs_node.children[0].value
+            self.variable_schemas[new_var_name] = self.variable_schemas.get(start_var_name)
+
+    # UPDATED: All join logic is now self-contained in this single method.
+    def join_expr(self, tree):
+        left_source_token, right_source_token, join_condition_node = tree.children
+        left_source_name = left_source_token.value
+        right_source_name = right_source_token.value
+
+        # 1. Check that sources are valid
+        for source in [left_source_name, right_source_name]:
+            if source not in self.variable_schemas:
+                raise ValueError(f"Validation Error: Variable '{source}' used in join is not defined or has no schema.")
+        
+        # 2. Build a local context for this join
+        context_schemas = {
+            left_source_name: self.schemas[self.variable_schemas[left_source_name]],
+            right_source_name: self.schemas[self.variable_schemas[right_source_name]]
+        }
+
+        # 3. Directly validate the join condition
+        left_col_ref = join_condition_node.children[0]
+        right_col_ref = join_condition_node.children[1]
+
+        for col_ref in [left_col_ref, right_col_ref]:
+            table_prefix = col_ref.children[0].value
+            column_name = col_ref.children[1].value
+
+            if table_prefix not in context_schemas:
+                raise ValueError(f"Validation Error: Unknown table alias '{table_prefix}' in join condition.")
+            if column_name not in context_schemas[table_prefix]:
+                raise ValueError(f"Validation Error: Column '{column_name}' not found in schema for '{table_prefix}'.")
+
     def pipeline(self, tree):
         start_var = tree.children[0].value
         schema_name = self.variable_schemas.get(start_var)
-        
-        previous_schema = self.current_schema
-        previous_schema_name = self.current_schema_name
+        previous_schema, previous_schema_name = self.current_schema, self.current_schema_name
         self.group_by_cols = []
-        
         if schema_name:
             self.current_schema = self.schemas.get(schema_name, {}).copy()
             self.current_schema_name = schema_name
         else:
-            self.current_schema = None
-            self.current_schema_name = "unknown"
-
+            self.current_schema, self.current_schema_name = None, "unknown"
         for child in tree.children:
-            if isinstance(child, Tree):
-                self.visit(child)
-        
-        self.current_schema = previous_schema
-        self.current_schema_name = previous_schema_name
+            if isinstance(child, Tree): self.visit(child)
+        self.current_schema, self.current_schema_name = previous_schema, previous_schema_name
 
+    def column_ref(self, tree):
+        # This generic check should NOT run during a join, as the context is different.
+        # The join_expr method now handles its own column validation.
+        if self.current_schema is None: return
+
+        # This check is for columns inside a pipeline (filter, mutate)
+        col_name = tree.children[1].value
+        if col_name not in self.current_schema:
+            raise ValueError(f"Validation Error: Column '{col_name}' not found in schema '{self.current_schema_name}'.")
+
+    # ... (rest of the file is unchanged, and the old join_condition method is gone) ...
     def group_by(self, tree):
         if not self.current_schema: return
         self.group_by_cols = []
         for col_name_node in tree.children:
             col_name = col_name_node.value
-            if col_name not in self.current_schema:
-                raise ValueError(f"Validation Error: Column '{col_name}' in group_by not found in schema '{self.current_schema_name}'.")
+            if col_name not in self.current_schema: raise ValueError(f"Validation Error: Column '{col_name}' in group_by not found in schema '{self.current_schema_name}'.")
             self.group_by_cols.append(col_name)
 
     def aggregate(self, tree):
         if not self.current_schema: return
         new_schema = {}
-        for col in self.group_by_cols:
-            new_schema[col] = self.current_schema.get(col, 'unknown')
+        for col in self.group_by_cols: new_schema[col] = self.current_schema.get(col, 'unknown')
         for agg_expr_node in tree.children:
             self.visit(agg_expr_node)
             new_col_name = agg_expr_node.children[0].value
@@ -57,28 +106,17 @@ class Validator(Visitor):
         for child in tree.children:
             if isinstance(child, Tree): self.visit(child)
             
-    # UPDATED: This method is now smarter and checks token types.
     def agg_function(self, tree):
         if not self.current_schema: return
-
-        # Iterate through children and only validate the column NAME, not the function name.
         for token in tree.children:
             if token.type == 'NAME':
                 col_name = token.value
-                if col_name not in self.current_schema:
-                    raise ValueError(f"Validation Error: Column '{col_name}' in aggregate function not found in schema '{self.current_schema_name}'.")
-
-    def column_ref(self, tree):
-        if not self.current_schema: return
-        col_name = tree.children[1].value
-        if col_name not in self.current_schema:
-            raise ValueError(f"Validation Error: Column '{col_name}' not found in schema '{self.current_schema_name}'.")
+                if col_name not in self.current_schema: raise ValueError(f"Validation Error: Column '{col_name}' in aggregate function not found in schema '{self.current_schema_name}'.")
 
     def mutate(self, tree):
         if self.current_schema is None: return
         for mutate_expr_node in tree.children:
-            if isinstance(mutate_expr_node, Tree) and len(mutate_expr_node.children) > 1:
-                self.visit(mutate_expr_node.children[1])
+            if isinstance(mutate_expr_node, Tree) and len(mutate_expr_node.children) > 1: self.visit(mutate_expr_node.children[1])
             new_col_name = mutate_expr_node.children[0].value
             self.current_schema[new_col_name] = 'dynamic'
 
@@ -102,8 +140,7 @@ class Validator(Visitor):
         if not self.current_schema: return
         for col_name_node in tree.children:
             col_name = col_name_node.value
-            if col_name not in self.current_schema:
-                raise ValueError(f"Validation Error: Column '{col_name}' not found in schema '{self.current_schema_name}'.")
+            if col_name not in self.current_schema: raise ValueError(f"Validation Error: Column '{col_name}' not found in schema '{self.current_schema_name}'.")
 
     def get_current_schema_name(self):
         return self.current_schema_name
